b7ab1ee71d29f14187d13b1a3b3952ddb34fa1d4
fallenk <kfallen@aliyun.com>
2016-11-25 16:25:34 +0800

    init iOS Project
    初始化工程，制作Demo学习

---
+++ Lighter-View-Controller/Lighter-View-Controller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lighter-View-Controller
+//
+//  Created by FallenK on 2016/11/25.
+//  Copyright © 2016年 KFallen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lighter-View-Controller/Lighter-View-Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lighter-View-Controller
+//
+//  Created by FallenK on 2016/11/25.
+//  Copyright © 2016年 KFallen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
